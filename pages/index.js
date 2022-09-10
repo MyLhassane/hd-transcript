d82043a889799f9c99a11aa55930f358f7f31ba9
My Lhassane <mastinoz_7@hotmail.com>
2022-09-10 20:29:32 +0100

    01. React Functional Component and useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Player from '../components/Player'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+    <Player />
   )
 }
