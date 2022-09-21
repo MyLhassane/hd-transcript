c1c494c78cfdb4f7ea89461d09ec6ce4beeb48ef
My Lhassane <mylhassanedeveloper@gmail.com>
2022-09-22 00:25:56 +0100

    Added Custom Controls

--- components/Player.jsx
+++ components/Player.jsx
@@ -5,26 +5,64 @@ const Player = () => {
     <div>
       <h2 className="h-12 py-3 ">HTML5 Player</h2>
 
-      <figure id="videoContainer"></figure>
+      <figure id="videoContainer">
+        <video
+          className="border-8 border-gray-700 rounded-md"
+          id="video"
+          width=""
+          height=""
+          controls
+          preload="metadata"
+          poster="./images/poster.jpeg"
+        >
+          <source src="./video/sintel-short.mp4" type="video/mp4" />
+          <source src="./video/sintel-short.webm" type="video/webm" />
+          <a href="./video/sintel-short.mp4">Download MP4</a>
+          Your browser does not support the video tag.
+        </video>
+        <figcaption>
+          &copy; Blender Foundation |
+          <a href="http://mango.blender.org">mango.blender.org</a>
+        </figcaption>
 
-      <video
-        className="border-8 border-gray-700 rounded-md"
-        id="video"
-        width=""
-        height=""
-        controls
-        preload="metadata"
-        poster="./images/poster.jpeg"
-      >
-        <source src="./video/sintel-short.mp4" type="video/mp4" />
-        <source src="./video/sintel-short.webm" type="video/webm" />
-        <a href="./video/sintel-short.mp4">Download MP4</a>
-        Your browser does not support the video tag.
-      </video>
-      <figcaption>
-        &copy; Blender Foundation |
-        <a href="http://mango.blender.org">mango.blender.org</a>
-      </figcaption>
+        <ul id="video-controls" className="controls float-left">
+          <li>
+            <button id="playpause" type="button">
+              Play/Pause
+            </button>
+          </li>
+          <li>
+            <button id="stop" type="button">
+              Stop
+            </button>
+          </li>
+          <li className="progress">
+            <progress id="progress" value="0" min="0">
+              <span id="progress-bar"></span>
+            </progress>
+          </li>
+          <li>
+            <button id="mute" type="button">
+              Mute/Unmute
+            </button>
+          </li>
+          <li>
+            <button id="volinc" type="button">
+              Vol+
+            </button>
+          </li>
+          <li>
+            <button id="voldec" type="button">
+              Vol-
+            </button>
+          </li>
+          <li>
+            <button id="fs" type="button">
+              Fullscreen
+            </button>
+          </li>
+        </ul>
+      </figure>
     </div>
   );
 };
